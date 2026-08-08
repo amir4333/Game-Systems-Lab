c7d1c9b8c3de2dc9d8febf0e0bdf9316037bd451
amir433 <abdollahiamir435@gmail.com>
2026-08-08 10:50:12 +0330

    improving and remaking DialogueSystem

--- systems/DialogueSystem/DialogueNode.py
+++ systems/DialogueSystem/DialogueNode.py
@@ -1,4 +1,8 @@
+from DialogueSystem import DialogueSystem
 class DialogueNode:
+
     def __init__(self, text, choices):
         self.text = text
         self.choices = choices
+
+        self.dialogue_system = DialogueSystem(self.text)
--- systems/DialogueSystem/DialogueSystem.py
+++ systems/DialogueSystem/DialogueSystem.py
@@ -1,21 +1,16 @@
-from DialogueChoice import DialogueChoice
-from DialogueNode import DialogueNode
-
 class DialogueSystem:
-    def __init__(self):
-        self.start_node
+    
+    def __init__(self, start_node):
+        self.start_node = start_node
+        self.current_node = start_node
 
-        self.second_choices = [DialogueChoice("Goodbye.")]
-        self.start_choices = [DialogueChoice("Who are you?", DialogueNode("I am the city guard.", self.second_choices)), DialogueChoice("Goodbye.")]
-        self.current_node = DialogueNode("Hello", self.start_choices)
+        # self.second_choices = [DialogueChoice("Goodbye.")]
+        # self.start_choices = [DialogueChoice("Who are you?", DialogueNode("I am the city guard.", self.second_choices)), DialogueChoice("Goodbye.")]
+        # self.current_node = DialogueNode("Hello", self.start_choices)
 
-    def Player_choice(self):
-        pass
-
-    def Go_to_next_node(self):
-
-        if len(self.dialogue_node.choices) != 0:
-            pass
+    def Go_to_next_node(self, choice_index):
+        if len(self.current_node.choices) != 0:
+            self.current_node = self.current_node.choices[choice_index].next_node
 
         if len(self.dialogue_node.choices) == 0:
             self.End_dialogues()
