d73185aeb55ae35a4ae08402c00c75efa745f2cc
amir433 <abdollahiamir435@gmail.com>
2026-08-16 11:33:47 +0330

    adding a list for completed quests and a set for better searching for all quests in QuestSystem

--- systems/QuestSystem/QuestSystem.py
+++ systems/QuestSystem/QuestSystem.py
@@ -1,18 +1,25 @@
 class QuestSystem:
     def __init__(self, active_quests):
         self.active_quests = active_quests
+        self.completed_quests = []
+        self.all_quests = set()
 
     def add_quest(self, quest):
-        if quest not in self.active_quests:
-            self.active_quests.append(quest)
+        if quest in self.all_quests:
             return
+        self.all_quests.add(quest)
+        self.active_quests.append(quest)
 
     def remove_quest(self, quest):
         if quest in self.active_quests:
             self.active_quests.remove(quest)
-            return
+
+    def send_to_completed(self, quest):
+        if quest in self.active_quests:
+                self.active_quests.remove(quest)
+                self.completed_quests.append(quest)
 
     def update(self):
         for quest in self.active_quests[:]:
             if quest.is_complete():
-                self.remove_quest(quest)
+                self.send_to_completed(quest)
