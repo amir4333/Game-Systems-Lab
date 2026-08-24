1c037b491bf41fc043ae88d8029f93a2c16775a0
amir433 <abdollahiamir435@gmail.com>
2026-08-24 17:33:08 +0330

    upgrading dialogue system

--- systems/DialogueSystem/DialogueChoice.py
+++ systems/DialogueSystem/DialogueChoice.py
@@ -1,5 +1,6 @@
 class DialogueChoice:
     
-    def __init__(self, text, next_node):
+    def __init__(self, text, next_node, action=None):
         self.text = text
         self.next_node = next_node
+        self.action = action
--- systems/DialogueSystem/DialogueSystem.py
+++ systems/DialogueSystem/DialogueSystem.py
@@ -6,6 +6,9 @@ class DialogueSystem:
 
     def choose(self, choice_index):
         if len(self.current_node.choices) != 0:
+            choice = self.current_node.choices[choice_index]
+            if choice.action:
+                choice.action()
             self.current_node = self.current_node.choices[choice_index].next_node
 
     def is_finished(self):
