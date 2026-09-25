1c70389835afa01229b559b4c8a30907511f6129
amir433 <abdollahiamir435@gmail.com>
2026-09-25 09:58:52 +0330

    updating inventory slots

--- systems/InventorySystem/InventorySlot.py
+++ systems/InventorySystem/InventorySlot.py
@@ -1,4 +1,4 @@
 class InventorySlot:
-    def __init__(self ,Item ,quantity=0):
-        self.item = Item
+    def __init__(self ,item_definition ,quantity=0):
+        self.item_definition = item_definition
         self.quantity = quantity
